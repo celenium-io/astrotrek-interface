aa2041876a0636fb42fe3e02ee18e03585e00ed4
GussevPM <GussevPM@GMail.Com>
2024-03-13 01:07:49 +0100

    Fill rollup widget

--- components/widgets/RollupsWidget.vue
+++ components/widgets/RollupsWidget.vue
@@ -1,19 +1,75 @@
+<script setup>
+/** Vendor */
+import { DateTime } from "luxon"
+
+/** UI */
+import Button from "~/components/ui/Button.vue"
+import Tooltip from "@/components/ui/Tooltip.vue"
+
+/** Services */
+import { comma, formatBytes, spaces } from "@/services/utils"
+
+/** API */
+import { fetchSeries } from "@/services/api/stats"
+
+/** Store */
+import { useAppStore } from "@/store/app"
+const appStore = useAppStore()
+
+const lastHead = computed(() => appStore.lastHead)
+
+const dataSizeSeries = ref([])
+const getDataSizeSeries = async () => {
+	const data = await fetchSeries({
+		table: "data_size",
+		period: "hour",
+		from: parseInt(DateTime.now().minus({ days: 1 }).ts / 1_000)
+	})
+	if (data) {
+		dataSizeSeries.value = data.sort((a, b) => new Date(a.time) - new Date(b.time))
+	}
+}
+
+await getDataSizeSeries()
+
+const totalDataSize = computed(() => dataSizeSeries.value.reduce((acc, curr) => acc + parseInt(curr.value), 0))
+const maxSize = computed(() => Math.max(...dataSizeSeries.value?.map((s) => s.value)))
+const calculateHeight = (size) => {
+	return 20 + (size / maxSize.value) * 80
+}
+
+</script>
+
 <template>
 	<Flex direction="column" justify="between" :class="$style.wrapper">
 		<Flex direction="column" gap="8">
 			<Text size="16" weight="600" color="primary"> Rollups Analysis </Text>
 
 			<Text size="14" weight="500" color="tertiary" height="140">
-				In the last <Text color="secondary">31</Text> days <Text color="secondary">12</Text> new rollups published
+				In the last <Text color="secondary">24</Text> hours <Text color="secondary">{{ formatBytes(totalDataSize) }}</Text> pushed
 			</Text>
 		</Flex>
 
-		<Flex align="center" gap="4" :class="$style.chart">
-			<Flex v-for="bar in 24" :class="$style.bar" />
+		<Flex align="end" gap="4" :class="$style.chart">
+			<Tooltip v-for="ds in dataSizeSeries" style="height: 100%">
+				<Flex
+					:class="$style.bar"
+					:style="{
+						height: `${calculateHeight(ds.value)}%`,
+					}"
+				/>
+
+				<template #content>
+					<Flex align="center" direction="column" gap="6">
+						<Text> {{ DateTime.fromISO(ds.time).setLocale("en").toFormat("t") }}</Text>
+						<Text color="tertiary">{{ formatBytes(ds.value) }}</Text>
+					</Flex>
+				</template>
+			</Tooltip>
 		</Flex>
 
 		<Flex direction="column" gap="8">
-			<Text size="28" weight="500" color="primary"> 17,720 </Text>
+			<Text size="28" weight="500" color="primary"> {{ comma(lastHead.total_rollups) }} </Text>
 			<Text size="12" weight="500" color="tertiary">Total Rollups</Text>
 		</Flex>
 	</Flex>
@@ -30,12 +86,30 @@
 	padding: 16px;
 }
 
+.chart {
+	height: 32px;
+
+	&:hover {
+		.bar:not(:hover) {
+			background: var(--txt-support);
+		}
+	}
+}
+
 .bar {
+	width: 4.5px;
+	
+	background: var(--brand);
+
+	transition: all 0.2s ease;
+}
+
+/* .bar {
 	width: 4px;
 	height: 16px;
 
 	background: var(--brand);
-}
+} */
 
 @media (max-width: 1000px) {
 	.wrapper {
