2b3d52818aa5b9a340162cf84bdd252e1b69edf3
GussevPM <GussevPM@GMail.Com>
2024-05-17 19:30:50 +0200

    Change API URLs

--- services/config.js
+++ services/config.js
@@ -1,10 +1,10 @@
 export const Server = {
 	API: {
-		mainnet: "https://api-dusk-5.astrotrek.io/v1",
+		mainnet: "https://api-dusk-test.astrotrek.io/v1",
 		dev: "https://api-dusk-test.astrotrek.io/v1",
 	},
 	WSS: {
-		mainnet: "wss://api-dusk-5.astrotrek.io/v1/ws",
+		mainnet: "wss://api-dusk-test.astrotrek.io/v1/ws",
 		dev: "wss://api-dusk-test.astrotrek.io/v1/ws",
 	},
 }
