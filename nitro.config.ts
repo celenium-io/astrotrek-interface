3d98000ba35711bfb81f7e599b10a8ed2d49354c
GussevPM <GussevPM@GMail.Com>
2024-06-18 00:02:32 +0200

    8 try (remove nitro storage)

--- nitro.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineNitroConfig({
-    "preset": "cloudflare-pages",
-    storage: {
-        db: {
-            driver: 'fs',
-            base: './storage/db'
-        }
-    }
-})
