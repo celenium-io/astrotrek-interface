7bc679626d92e2c256303519bf0bf2e34ba0d8e4
GussevPM <GussevPM@GMail.Com>
2025-09-20 16:40:30 +0200

    Upd legal

--- services/constants/legal.js
+++ services/constants/legal.js
@@ -1,3 +1,3 @@
-export const PRIVACY_POLICY_DATE = "2024-12-01"
+export const PRIVACY_POLICY_DATE = "2025-09-15"
 
-export const TERMS_OF_USE_DATE = "2024-12-01"
+export const TERMS_OF_USE_DATE = "2025-09-15"
